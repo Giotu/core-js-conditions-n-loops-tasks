b7cade5ddf4d64291e432d92da9754fc80823732
Giotu <evilalex1998@mail.ru>
2024-01-16 03:36:51 +0300

    feat: resolve task getIndexOf

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -202,8 +202,14 @@ function isPalindrome(str) {
  *  'qwerty', 'Q'     => -1
  *  'qwerty', 'p'     => -1
  */
-function getIndexOf(/* str, letter */) {
-  throw new Error('Not implemented');
+function getIndexOf(str, letter) {
+  let result = -1;
+  for (let i = 0; i < str.length; i += 1) {
+    if (letter === str[i]) {
+      result = i;
+    }
+  }
+  return result;
 }
 
 /**
